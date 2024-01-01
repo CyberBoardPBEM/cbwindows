6c5be32ef4c354a6a829a4b1bf2272a3086028a8
Bill Su <wsu@acm.org>
2024-01-01 17:36:40 -0500

    check for build failures

--- build-all-configs.bat
+++ build-all-configs.bat
@@ -11,20 +11,28 @@ if errorlevel 1 goto fail_vc_x64
 echo on
 
 cmake -G Ninja -B out/build/x64-Debug -DCMAKE_BUILD_TYPE=Debug
+if errorlevel 1 goto fail_cmake
 cmake --build out/build/x64-Debug
+if errorlevel 1 goto fail_build
 
 cmake -G Ninja -B out/build/x64-Release -DCMAKE_BUILD_TYPE=RelWithDebInfo
+if errorlevel 1 goto fail_cmake
 cmake --build out/build/x64-Release
+if errorlevel 1 goto fail_build
 
 call %VSROOTDIR%"\2022\Community\VC\Auxiliary\Build\vcvars32.bat"
 if errorlevel 1 goto fail_vc_x86
 echo on
 
 cmake -G Ninja -B out/build/x86-Debug -DCMAKE_BUILD_TYPE=Debug
+if errorlevel 1 goto fail_cmake
 cmake --build out/build/x86-Debug
+if errorlevel 1 goto fail_build
 
 cmake -G Ninja -B out/build/x86-Release -DCMAKE_BUILD_TYPE=RelWithDebInfo
+if errorlevel 1 goto fail_cmake
 cmake --build out/build/x86-Release
+if errorlevel 1 goto fail_build
 
 goto done
 
@@ -34,6 +42,14 @@ goto done
 
 :fail_vc_x86
 echo vcvars32.bat not found
+goto done
+
+:fail_cmake
+echo cmake failed
+goto done
+
+:fail_build
+echo cmake --build failed
 goto done
 
 :done
