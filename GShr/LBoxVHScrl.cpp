7d4b40a6c3e41aa4b5b36d1ac389c8b8c22e3883
Bill Su <wsu@acm.org>
2024-01-06 15:34:47 -0500

    extend wxVListBox with horizontal scrolling

---
+++ GShr/LBoxVHScrl.cpp
@@ -0,0 +1,168 @@
+// LBoxVHScrl.cpp
+//
+// Copyright (c) 2024 By William Su, All Rights Reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#include "stdafx.h"
+
+#include "LBoxVHScrl.h"
+
+namespace CB
+{
+    wxIMPLEMENT_ABSTRACT_CLASS(VListBoxHScroll, wxVListBox);
+
+    wxBEGIN_EVENT_TABLE(VListBoxHScroll, wxVListBox)
+        EVT_SCROLLWIN(VListBoxHScroll::OnScrollWin)
+    wxEND_EVENT_TABLE()
+
+    VListBoxHScroll::VListBoxHScroll()
+    {
+        SetBackgroundStyle(wxBG_STYLE_PAINT);
+    }
+
+    void VListBoxHScroll::SetItemCount(size_t count)
+    {
+        wxVListBox::SetItemCount(count);
+        wxSize virtSize = GetVirtualSize();
+        wxCoord width = virtSize.GetWidth();
+        for (size_t i = 0 ; i < count ; ++i)
+        {
+            width = std::max(width, GetItemSize(i).GetWidth());
+        }
+        SetVirtualSize(width, virtSize.GetHeight());
+    }
+
+    // logical --> device
+    wxCoord VListBoxHScroll::CalcScrolledX(wxCoord x) const
+    {
+        return x - GetScrollPos(wxHORIZONTAL);
+    }
+
+    // device --> logical
+    wxCoord VListBoxHScroll::CalcUnscrolledX(wxCoord x) const
+    {
+        return x + GetScrollPos(wxHORIZONTAL);
+    }
+
+    void VListBoxHScroll::DoSetVirtualSize(int x, int y)
+    {
+        wxVListBox::DoSetVirtualSize(x, y);
+        wxSize clientSize = GetClientSize();
+        SetScrollbar(wxHORIZONTAL, 0, clientSize.GetWidth(), x);
+    }
+
+    void VListBoxHScroll::OnScrollWin(wxScrollWinEvent& event)
+    {
+        wxEventType evt = event.GetEventType();
+#if !defined(NDEBUG)
+        const char* name;
+        if (evt == wxEVT_SCROLLWIN_TOP)
+        {
+            name = "wxEVT_SCROLLWIN_TOP";
+        }
+        else if (evt == wxEVT_SCROLLWIN_BOTTOM)
+        {
+            name = "wxEVT_SCROLLWIN_BOTTOM";
+        }
+        else if (evt == wxEVT_SCROLLWIN_LINEUP)
+        {
+            name = "wxEVT_SCROLLWIN_LINEUP";
+        }
+        else if (evt == wxEVT_SCROLLWIN_LINEDOWN)
+        {
+            name = "wxEVT_SCROLLWIN_LINEDOWN";
+        }
+        else if (evt == wxEVT_SCROLLWIN_PAGEUP)
+        {
+            name = "wxEVT_SCROLLWIN_PAGEUP";
+        }
+        else if (evt == wxEVT_SCROLLWIN_PAGEDOWN)
+        {
+            name = "wxEVT_SCROLLWIN_PAGEDOWN";
+        }
+        else if (evt == wxEVT_SCROLLWIN_THUMBTRACK)
+        {
+            name = "wxEVT_SCROLLWIN_THUMBTRACK";
+        }
+        else if (evt == wxEVT_SCROLLWIN_THUMBRELEASE)
+        {
+            name = "wxEVT_SCROLLWIN_THUMBRELEASE";
+        }
+        else
+        {
+            wxASSERT(!"unknown event type");
+            name = "???";
+        }
+        CPP20_TRACE(L"{}({}, {}, {})\n", __func__, name, event.GetOrientation() == wxHORIZONTAL ? "horz" : "vert", event.GetPosition());
+#endif
+
+        if (event.GetOrientation() == wxVERTICAL)
+        {
+            event.Skip();
+            return;
+        }
+
+        int width = GetClientSize().GetWidth();
+        int maxWidth = GetScrollRange(wxHORIZONTAL);
+        int pos = GetScrollPos(wxHORIZONTAL);
+        if (evt == wxEVT_SCROLLWIN_TOP)
+        {
+            pos = 0;
+        }
+        else if (evt == wxEVT_SCROLLWIN_BOTTOM)
+        {
+            pos = maxWidth;
+        }
+        else if (evt == wxEVT_SCROLLWIN_LINEUP)
+        {
+            pos -= width / 10;
+        }
+        else if (evt == wxEVT_SCROLLWIN_LINEDOWN)
+        {
+            pos += width / 10;
+        }
+        else if (evt == wxEVT_SCROLLWIN_PAGEUP)
+        {
+            pos -= width * 9 / 10;
+        }
+        else if (evt == wxEVT_SCROLLWIN_PAGEDOWN)
+        {
+            pos += width * 9 / 10;
+        }
+        else if (evt == wxEVT_SCROLLWIN_THUMBTRACK)
+        {
+            pos = event.GetPosition();
+        }
+        else if (evt == wxEVT_SCROLLWIN_THUMBRELEASE)
+        {
+            pos = event.GetPosition();
+        }
+        else
+        {
+            wxASSERT(!"unknown event type");
+        }
+        pos = std::max(0, pos);
+        pos = std::min(pos, maxWidth);
+        SetScrollPos(wxHORIZONTAL, pos);
+        Refresh();
+    }
+}
---
+++ GShr/LBoxVHScrl.h
@@ -0,0 +1,74 @@
+// LBoxVHScrl.h
+//
+// Copyright (c) 2024 By William Su, All Rights Reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#ifndef _LBOXVHSCRL_H
+#define _LBOXVHSCRL_H
+
+namespace CB
+{
+    class VListBoxHScroll : public wxVListBox
+    {
+    public:
+        VListBoxHScroll();
+
+        void SetItemCount(size_t count) override;
+
+        // the derived class must implement this function to actually draw the item
+        // with the given index on the provided DC
+        void OnDrawItem(wxDC& dc, const wxRect& rect, size_t n) const override = 0;
+
+        // the derived class must implement this method to return the height of the
+        // specified item
+        wxCoord OnMeasureItem(size_t n) const override
+        {
+            return GetItemSize(n).GetHeight();
+        }
+
+        virtual wxSize GetItemSize(size_t n) const = 0;
+
+        // logical --> device
+        wxCoord CalcScrolledX(wxCoord x) const;
+        wxPoint CalcScrolledPosition(const wxPoint& pt) const
+        {
+            return wxPoint(CalcScrolledX(pt.x), pt.y);
+        }
+        // device --> logical
+        wxCoord CalcUnscrolledX(wxCoord x) const;
+        wxPoint CalcUnscrolledPosition(const wxPoint& pt) const
+        {
+            return wxPoint(CalcUnscrolledX(pt.x), pt.y);
+        }
+
+    protected:
+        void DoSetVirtualSize(int x, int y) override;
+
+    private:
+        wxDECLARE_ABSTRACT_CLASS(VListBoxHScroll);
+        wxDECLARE_EVENT_TABLE();
+
+        void OnScrollWin(wxScrollWinEvent& event);
+    };
+}
+
+#endif
